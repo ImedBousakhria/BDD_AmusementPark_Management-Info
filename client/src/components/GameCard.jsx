215a1145577e8a5aa7e3c5e475c627788986286e
ImedBousakhria <i_bousakhria@estin.dz>
2024-01-24 12:37:28 +0100

    feat: add new components to the Attractions page

--- client/src/components/GameCard.jsx
+++ client/src/components/GameCard.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import game from "../assets/icons/game.svg";
 import moredots from "../assets/icons/moredots.svg";
-import Rating from "@mui/material/Rating";
 import EditDeleteActions from "./super/EditDeleteActions";
+import GameDetails from "./GameDetails";
 
-const GameCard = ({ attraction }) => {
+const GameCard = ({ attraction, onClick }) => {
   const [showActions, setShowActions] = useState(false);
 
   const handleMoreDotsClick = () => {
@@ -37,7 +37,7 @@ const GameCard = ({ attraction }) => {
       )}
       <div className="flex w-[100%]">
         <img src={game} alt="" />
-        <Details game={attraction} />
+        <GameDetails game={attraction} />
       </div>
       <small className="text-customPink font-medium cursor-pointer place-self-end m-3 ">
         See in map
@@ -48,41 +48,3 @@ const GameCard = ({ attraction }) => {
 
 export default GameCard;
 /* HERE */
-const Details = ({ game, userType }) => {
-  const [value, setValue] = useState(game.rating);
-  const { title, ageRating, status } = game;
-
-  let statusColorClass = "";
-
-  switch (status) {
-    case "Open":
-      statusColorClass = "text-green-600";
-      break;
-    case "Closed":
-      statusColorClass = "text-red-600";
-      break;
-    case "Under Maintenance":
-      statusColorClass = "text-orange-600";
-      break;
-    default:
-      statusColorClass = "";
-  }
-
-  return (
-    <div className="flex flex-col p-3">
-      <h3 className="overflow-hidden font-semibold">{title}</h3>
-      <Rating
-      className=" "
-        name="simple-controlled"
-        value={value}
-        readOnly={userType}
-        onChange={(event, newValue) => {
-          setValue(newValue);
-        }}
-      />
-      <small className="text-gray-400 mt-4">Attraction</small>
-      <small className="text-gray-500">{ageRating}</small>
-      <p className={` ${statusColorClass} font-semibold`}>{status}</p>
-    </div>
-  );
-};
